823cd9146af87f117a8e9f5bfdfe950aac5816cc
balladeno24 <josephdonnelly2024@outlook.com>
2025-12-29 23:42:57 +0000

    Remove EnableVaadin to allow Vaadin route scanning

--- src/main/kotlin/org/example/MainApplication.kt
+++ src/main/kotlin/org/example/MainApplication.kt
@@ -1,11 +1,9 @@
 package org.example
 
-import com.vaadin.flow.spring.annotation.EnableVaadin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableVaadin
 open class MainApplication
 
 fun main(args: Array<String>) {
